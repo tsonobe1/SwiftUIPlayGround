46511ab718a73e73b8b4c250c31faf2fd25e3aa8
tsonobe <sono.tak.st@gmail.com>
2022-06-18 10:36:27 +0900

    Initial Commit

---
+++ SwiftUIPlayGround/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIPlayGround
+//
+//  Created by 薗部拓人 on 2022/06/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIPlayGround/SwiftUIPlayGroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPlayGroundApp.swift
+//  SwiftUIPlayGround
+//
+//  Created by 薗部拓人 on 2022/06/18.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPlayGroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
