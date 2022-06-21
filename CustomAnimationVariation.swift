e0153f9a94ecb2aae01e17391e452c1c16f561c5
tsonobe <sono.tak.st@gmail.com>
2022-06-21 16:20:38 +0900

    fix: animation variation fixed

--- CustomAnimationVariation.swift
+++ CustomAnimationVariation.swift
@@ -15,100 +15,108 @@ struct CustomAnimationVariation: View {
             Button(action: {self.on.toggle()}) {
                 Text("Toggle")
             }
-            ScrollView{
-                VStack{
-                    Group{
-                        Text("linear")
-                        Image(systemName: "arrow.up.circle.fill")
-                            .frame(width: 300, alignment: on ? .leading : .trailing)
-                            .font(.system(size: 40))
-                            .animation(.linear, value: on)
-                        
-                        
-                        Image(systemName: "arrow.up.circle.fill")
-                            .frame(width: 300, alignment: on ? .leading : .trailing)
-                            .font(.system(size: 40))
-                            .animation(.linear(duration: 0.35), value: on)
-                    }
+            
+            VStack(alignment: .leading){
+                Group{
+                    Text("linear").bold()
+                    Image(systemName: on ? "arrow.right.circle.fill" : "arrow.left.circle.fill")
+                        .frame(width: 300, alignment: on ? .leading : .trailing)
+                        .font(.system(size: 40))
+                        .foregroundColor(Color(UIColor.rand1))
+                        .animation(.linear, value: on)
                     
-                    Group{
-                        Text("easeIn")
-                        Image(systemName: "arrow.up.circle.fill")
-                            .frame(width: 300, alignment: on ? .leading : .trailing)
-                            .font(.system(size: 40))
-                            .animation(.easeIn, value: on)
-                        
-                        
-                        Image(systemName: "arrow.up.circle.fill")
-                            .frame(width: 300, alignment: on ? .leading : .trailing)
-                            .font(.system(size: 40))
-                            .animation(.easeIn(duration: 0.35), value: on)
-                    }
                     
-                    Group{
-                        Text("easeInOut")
-                        Image(systemName: "arrow.up.circle.fill")
-                            .frame(width: 300, alignment: on ? .leading : .trailing)
-                            .font(.system(size: 40))
-                            .animation(.easeInOut, value: on)
-                        
-                        
-                        Image(systemName: "arrow.up.circle.fill")
-                            .frame(width: 300, alignment: on ? .leading : .trailing)
-                            .font(.system(size: 40))
-                            .animation(.easeInOut(duration: 0.35), value: on)
-                    }
+                    //                        Image(systemName: on ? "arrow.right.circle.fill" : "arrow.left.circle.fill")
+                    //                            .frame(width: 300, alignment: on ? .leading : .trailing)
+                    //                            .font(.system(size: 40))
+                    //                            .animation(.linear(duration: 2), value: on)
+                }
+                
+                Group{
+                    Text("easeIn").bold()
+                    Image(systemName: on ? "arrow.right.circle.fill" : "arrow.left.circle.fill")
+                        .frame(width: 300, alignment: on ? .leading : .trailing)
+                        .font(.system(size: 40))
+                        .foregroundColor(Color(UIColor.rand1))
+                        .animation(.easeIn, value: on)
                     
-                    Group{
-                        Text("easeOut")
-                        Image(systemName: "arrow.up.circle.fill")
-                            .frame(width: 300, alignment: on ? .leading : .trailing)
-                            .font(.system(size: 40))
-                            .animation(.easeOut, value: on)
-                        
-                        
-                        Image(systemName: "arrow.up.circle.fill")
-                            .frame(width: 300, alignment: on ? .leading : .trailing)
-                            .font(.system(size: 40))
-                            .animation(.easeOut(duration: 0.35), value: on)
-                    }
                     
-                    Group{
-                        Text("spring")
-                        Image(systemName: "arrow.up.circle.fill")
-                            .frame(width: 300, alignment: on ? .leading : .trailing)
-                            .font(.system(size: 40))
-                            .animation(.spring(), value: on)
-                        
-                        
-                        Image(systemName: "arrow.up.circle.fill")
-                            .frame(width: 300, alignment: on ? .leading : .trailing)
-                            .font(.system(size: 40))
-                            .animation(.spring(response: 0.5, dampingFraction: 3), value: on)
-                    }
+                    //                        Image(systemName: on ? "arrow.right.circle.fill" : "arrow.left.circle.fill")
+                    //                            .frame(width: 300, alignment: on ? .leading : .trailing)
+                    //                            .font(.system(size: 40))
+                    //                            .animation(.easeIn(duration: 0.35), value: on)
+                }
+                
+                Group{
+                    Text("easeInOut").bold()
+                    Image(systemName: on ? "arrow.right.circle.fill" : "arrow.left.circle.fill")
+                        .frame(width: 300, alignment: on ? .leading : .trailing)
+                        .font(.system(size: 40))
+                        .foregroundColor(Color(UIColor.rand1))
+                        .animation(.easeInOut, value: on)
                     
-                    Group{
-                        Text("interactiveSpring")
-                        Image(systemName: "arrow.up.circle.fill")
-                            .frame(width: 300, alignment: on ? .leading : .trailing)
-                            .font(.system(size: 40))
-                            .animation(.interactiveSpring(), value: on)
-                        
-                        
-                        Image(systemName: "arrow.up.circle.fill")
-                            .frame(width: 300, alignment: on ? .leading : .trailing)
-                            .font(.system(size: 40))
-                            .animation(.interactiveSpring(response: 0.5, dampingFraction: 3), value: on)
-                    }
                     
-                    Group{
-                        Text("interpolatingSpring")
-                        Image(systemName: "arrow.up.circle.fill")
-                            .frame(width: 300, alignment: on ? .leading : .trailing)
-                            .font(.system(size: 40))
-                            .animation(.interpolatingSpring(mass: 0.2, stiffness: 1, damping: 2, initialVelocity: 5), value: on)
-                        
-                    }
+                    //                        Image(systemName: on ? "arrow.right.circle.fill" : "arrow.left.circle.fill")
+                    //                            .frame(width: 300, alignment: on ? .leading : .trailing)
+                    //                            .font(.system(size: 40))
+                    //                            .animation(.easeInOut(duration: 0.35), value: on)
+                }
+                
+                Group{
+                    Text("easeOut").bold()
+                    Image(systemName: on ? "arrow.right.circle.fill" : "arrow.left.circle.fill")
+                        .frame(width: 300, alignment: on ? .leading : .trailing)
+                        .font(.system(size: 40))
+                        .foregroundColor(Color(UIColor.rand1))
+                        .animation(.easeOut, value: on)
+                    
+                    
+                    
+                    //                        Image(systemName: on ? "arrow.right.circle.fill" : "arrow.left.circle.fill")
+                    //                            .frame(width: 300, alignment: on ? .leading : .trailing)
+                    //                            .font(.system(size: 40))
+                    //                            .animation(.easeOut(duration: 0.35), value: on)
+                }
+                
+                Group{
+                    Text("spring").bold()
+                    Image(systemName: on ? "arrow.right.circle.fill" : "arrow.left.circle.fill")
+                        .frame(width: 300, alignment: on ? .leading : .trailing)
+                        .font(.system(size: 40))
+                        .foregroundColor(Color(UIColor.rand1))
+                        .animation(.spring(), value: on)
+                    
+                    
+                    Image(systemName: on ? "arrow.right.circle.fill" : "arrow.left.circle.fill")
+                        .frame(width: 300, alignment: on ? .leading : .trailing)
+                        .font(.system(size: 40))                            .foregroundColor(Color(UIColor.rand1))
+                        .animation(.spring(response: 0.5, dampingFraction: 3), value: on)
+                }
+                
+                Group{
+                    Text("interactiveSpring").bold()
+                    Image(systemName: on ? "arrow.right.circle.fill" : "arrow.left.circle.fill")
+                        .frame(width: 300, alignment: on ? .leading : .trailing)
+                        .font(.system(size: 40))
+                        .foregroundColor(Color(UIColor.rand1))
+                        .animation(.interactiveSpring(), value: on)
+                    
+                    
+                    Image(systemName: on ? "arrow.right.circle.fill" : "arrow.left.circle.fill")
+                        .frame(width: 300, alignment: on ? .leading : .trailing)
+                        .font(.system(size: 40))
+                        .foregroundColor(Color(UIColor.rand1))
+                        .animation(.interactiveSpring(response: 0.5, dampingFraction: 3), value: on)
+                }
+                
+                Group{
+                    Text("interpolatingSpring").bold()
+                    Image(systemName: on ? "arrow.right.circle.fill" : "arrow.left.circle.fill")
+                        .frame(width: 300, alignment: on ? .leading : .trailing)
+                        .font(.system(size: 40))
+                        .foregroundColor(Color(UIColor.rand1))
+                        .animation(.interpolatingSpring(mass: 0.2, stiffness: 2, damping: 1, initialVelocity: 5), value: on)
+                    
                 }
             }
         }
